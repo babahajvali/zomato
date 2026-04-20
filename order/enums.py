466086f19ebe17e820e7b12415e3fc4f9a5cc61c
Baba <babasvali2121@gmail.com>
2026-04-20 18:51:54 +0530

    add the order models

--- order/enums.py
+++ order/enums.py
@@ -8,3 +8,15 @@ class PromoCodeType(Enum):
     @classmethod
     def get_list_of_tuples(cls):
         return [(member.value, member.value.capitalize()) for member in cls]
+
+class OrderStatus(Enum):
+    PLACED = "PLACED"
+    CONFIRMED = "CONFIRMED"
+    PREPARING = "PREPARING"
+    OUT_OF_DELIVERY = "OUT_OF_DELIVERY"
+    DELIVERED = "DELIVERED"
+    CANCELLED = "CANCELLED"
+
+    @classmethod
+    def get_list_of_tuples(cls):
+        return [(member.value, member.value.capitalize()) for member in cls]
